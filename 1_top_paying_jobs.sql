84cfea4500586f0a4982078e1e74f38b32cf215b
manav953 <manavm@iitk.ac.in>
2026-08-15 19:09:01 +0530

    push vcc

--- 1_top_paying_jobs.sql
+++ 1_top_paying_jobs.sql
@@ -1,3 +1,5 @@
+-- Active: 1786432154371@@127.0.0.1@5433@postgres
+-- Active: 1786431846230@@127.0.0.1@5433@sql_course@public
 
 SELECT salary_year_avg, job_title, job_id,job_posted_date, job_schedule_type, job_location,name AS company_name
 FROM job_postings_fact
@@ -5,3 +7,5 @@ LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL AND job_location = 'India'
 ORDER BY salary_year_avg DESC
 LIMIT 10
+
+
--- 2_top_paying_job_skills.sql
+++ 2_top_paying_job_skills.sql
@@ -1,8 +1,15 @@
 
 
+WITH top10_jobs AS (
 SELECT salary_year_avg, job_title, job_id,job_posted_date, job_schedule_type, job_location,name AS company_name
 FROM job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL AND job_location = 'India'
 ORDER BY salary_year_avg DESC
 LIMIT 10
+)
+SELECT top10_jobs.*,skills
+FROM top10_jobs
+INNER JOIN skills_job_dim ON top10_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC
