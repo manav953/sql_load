c79721538b45f0a680583c7ac125947542f435dc
manav953 <manavm@iitk.ac.in>
2026-08-15 19:49:33 +0530

    push qw

--- 3_top_demanded_skills.sql
+++ 3_top_demanded_skills.sql
@@ -1,2 +1,11 @@
 
-SELECT *
+SELECT 
+       COUNT(skills_job_dim.job_id) AS demand,
+       skills
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short = 'Data Analyst' AND job_location ='India' 
+GROUP BY skills
+ORDER BY demand DESC
+LIMIT 5 
