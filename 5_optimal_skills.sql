4abcb8acdc755640c25c04582c939e45c457be70
manav953 <manavm@iitk.ac.in>
2026-08-15 21:02:10 +0530

    push cac

--- 5_optimal_skills.sql
+++ 5_optimal_skills.sql
@@ -1,11 +1,32 @@
 
-
-SELECT  
-COUNT(skills_job_dim.job_id) AS demand,skills
-FROM job_postings_fact
-INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE job_title_short = 'Data Analyst' AND job_location ='India' 
-GROUP BY skills
-ORDER BY demand DESC
-LIMIT 5 
+WITH skills_demand AS (
+    SELECT 
+       COUNT(skills_job_dim.job_id) AS demand
+       ,skills_job_dim.skill_id
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst' AND job_location ='India' AND salary_year_avg IS NOT NULL 
+    GROUP BY skills_job_dim.skill_id,skills
+    ORDER BY demand DESC
+    
+),average_salary AS (
+    SELECT 
+    ROUND(AVG(salary_year_avg), 2) AS avg_salary,skills_job_dim.skill_id
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst' AND job_location ='India' AND salary_year_avg IS NOT NULL 
+    GROUP BY skills_job_dim.skill_id,skills
+    ORDER BY avg_salary DESC
+    
+)
+ 
+SELECT
+    skills_demand.skill_id,
+    skills_demand.skill,
+    demand,
+    average_salary
+FROM
+    skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
