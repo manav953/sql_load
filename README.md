065b19456a883bec248ab61ed6b78f86e9d9e7cd
manav953 <manavm@iitk.ac.in>
2026-08-15 21:11:51 +0530

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Update the contents of this later
+
+# INTRODUCTION
+# BACKGROUND
+# Tools I Used
+# The Analysis
+# What I Learned
--- query1.sql
+++
@@ -1,6 +0,0 @@
-
-SELECT * 
-FROM( 
-    (SELECT * FROM job_postings_fact 
-    WHERE EXTRACT(MONTH FROM job_posted_date) = 1) 
-) AS january_jobs;
