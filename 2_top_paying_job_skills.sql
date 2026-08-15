58cb5a625ec91c1648b470451133d5c13dbb7cb5
manav953 <manavm@iitk.ac.in>
2026-08-15 17:52:09 +0530

    commit ban

--- 2_top_paying_job_skills.sql
+++ 2_top_paying_job_skills.sql
@@ -1,4 +1,5 @@
 
+
 SELECT salary_year_avg, job_title, job_id,job_posted_date, job_schedule_type, job_location,name AS company_name
 FROM job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
